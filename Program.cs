45c50ac60782ef9f25e7a3035f40d34600fe14cd
Naledi Tshiqi <g17t6451@ru.ac.za>
2019-02-24 00:49:49 +0200

    Third commit.After creating nunit test.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,50 @@
 
 namespace MyStuff
 {
-    class Program
+    public class Program
     {
+        public static string Prefix(string str)
+        {
+            int i, wrd, l;
+
+
+
+
+
+            l = 0;
+            wrd = 1;
+
+            /* loop till end of string */
+            while (l <= str.Length - 1)
+            {
+                /* check whether the current character is white space or new line or tab character*/
+                if (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
+                {
+                    wrd++;
+                }
+
+                l++;
+            }
+
+
+
+            int count = 0;
+            foreach (Char c in str)
+            {
+
+                count++;
+
+            }
+
+            string output = $"{wrd},{count}: {str}";
+            //Console.Write("Output:{0} and {1} : {2} ", wrd, count, str);
+            //Console.ReadLine();
+            return output;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Prefix("123 12"));
+            Console.ReadLine();
         }
     }
 }
